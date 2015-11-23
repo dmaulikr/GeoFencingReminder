d60f6a3e1ccefc8bb3a8872090583b7224a0ac0f
Lynn Kuhlman <kuhlman.lynn@gmail.com>
2015-11-23 12:52:21 -0800

    Initial Commit

---
+++ Reminder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Reminder
+//
+//  Created by Regular User on 11/23/15.
+//  Copyright © 2015 Lynn Kuhlman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Reminder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Reminder
+//
+//  Created by Regular User on 11/23/15.
+//  Copyright © 2015 Lynn Kuhlman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
